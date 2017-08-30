8870e849cedf7e98fb3bbe60e47a5e13e9d616a1
Mel Huang <hello@melaniehuang.com>
2017-08-30 22:10:33 +1000

    starting files

---
+++ js/minerals.js
@@ -0,0 +1,22 @@
+function preload(){
+  t = loadImage("images/pour-grey.gif");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight, WEBGL);
+}
+
+function draw(){
+  background("#cecece");
+
+  translate(0,-100,0);
+  texture(t);
+  for (var i = 0; i < 15; i++){
+    noStroke();
+    translate(i,i,12);
+    rotateY(frameCount*0.005);
+    rotateX(frameCount*0.01);
+    cylinder(8+i, 200);
+  }
+
+}
