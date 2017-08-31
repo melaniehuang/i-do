9ca4940c83192f0bb9150fa8106ccf6027c4a4a5
Mel Huang <hello@melaniehuang.com>
2017-08-31 14:15:22 +1000

    generating word combos

--- js/ido.js
+++ js/ido.js
@@ -1,1 +1,23 @@
 //LOVE IS LOVE, LOVE IS HUMAN, HUMAN IS HUMAN
+// 0 1 2 and then 0 1
+
+var words = [
+	["Love", "Love"], 
+	["Love", "Human"], 
+	["Human", "Human"]
+];
+
+var count = 0;
+
+setInterval(function(){
+	changeWords();
+}, 2000);
+
+function changeWords(){
+  document.getElementById("firstWord").innerHTML = words[count][0];
+  document.getElementById("lastWord").innerHTML = words[count][1];
+  count++;
+  if (count == 3){
+  	count = 0;
+  }
+};
